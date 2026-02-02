e7d49af545c5788e78e1dcde6620af4d8d6dbc38
bhavdipsinh parmar <bhavdipsinhparmar245@gmail.com>
2026-02-02 18:36:38 +0530

    main

---
+++ python/excercise/loop/7_armstrong .py
@@ -0,0 +1,15 @@
+'''7) write a program to figure out whether given number  is armstrong number or not'''
+
+number=int(input("enter any number..."))
+
+temp=number
+sum=0
+while temp>0:
+     digit=temp % 10
+     sum=sum+digit**3
+     temp=temp//10
+if sum==number:
+     print("this number is armstrong number...")
+else:
+     print("this number is not armstrong number ...")
+
---
+++ python/excercise/loop/8_numeric list.py
@@ -0,0 +1,14 @@
+''' write a program to count odd and even number in numeric list '''
+
+num_list=[10,23,45,66,60,20,55,33,78,90]
+
+odd_count=0
+even_count=0
+
+for num in num_list:
+     if num % 2==0:
+          even_count+=1
+     else:
+          odd_count+=1
+print("total even number is :",even_count)
+print("total odd number is :",odd_count)
