a48a29180d49a6bd9d4600be550646ff67078b85
bhavdipsinh parmar <bhavdipsinhparmar245@gmail.com>
2026-02-03 19:21:16 +0530

    loop

---
+++ python/14_nested_loop_1.py
@@ -0,0 +1,12 @@
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+Row = 6
+for i in range(1,6):
+     for i in range(i,Row):
+          print("*", end=" ")
+     print()
---
+++ python/excercise/loop/9_nested 1.py
@@ -0,0 +1,10 @@
+'''1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5'''
+
+for i in range(1,6):
+     for i in range(i):
+         print(i+1, end=" ")
+     print()     
